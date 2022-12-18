d5d00a42dfd28b6ae1f6a4f34fb22153be9de17c
Oleksandr Omelchenko <alexmir_98@ukr.net>
2022-12-18 15:49:58 +0200

    update

--- src/main/java/org/example/StringManager.java
+++ src/main/java/org/example/StringManager.java
@@ -3,7 +3,7 @@ package org.example;
 public class StringManager {
 
     public String getReverseUppercaseString(String string) {
-        StringBuilder stringBuilder = new StringBuilder(string.toLowerCase());
+        StringBuilder stringBuilder = new StringBuilder(string.toUpperCase());
 
         return stringBuilder.reverse().toString();
     }
