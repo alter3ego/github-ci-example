e9a5578988a5397341546dccd00dad0855422731
Oleksandr Omelchenko <alexmir_98@ukr.net>
2022-12-18 15:47:50 +0200

    error5

--- src/main/java/org/example/StringManager.java
+++ src/main/java/org/example/StringManager.java
@@ -5,7 +5,6 @@ public class StringManager {
     public String getReverseUppercaseString(String string) {
         StringBuilder stringBuilder = new StringBuilder(string.toLowerCase());
 
-        String string1 = stringBuilder.reverse().toString();
-        return string1;
+        return stringBuilder.reverse().toString();
     }
 }
